d140aa8231d21e136ca468e9f9d3976ec18276e3
Zeta <ag.sebastian912@gmail.com>
2024-06-19 20:23:41 -0300

    Updates

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 var menuBtn = document.querySelector('.menu-btn')
 var navLinksWrapper = document.querySelector('.nav-links')
+var navLinks = document.querySelectorAll('.nav-links li a')
 
 menuBtn.addEventListener('click', toggleMenu);
 //Function To Toggle Menu Button
 function toggleMenu(){
     menuBtn.classList.toggle('active');
     navLinksWrapper.classList.toggle('active');
+}
+
+for(var i = 0; i < navLinks.length; i++){
+    navLinks[i].addEventListener('click', toggleMenu)
 }
